b62eec9aeb680a822d92c73395edf52985c0c854
onkargurav <onkargurav5131@gmail.com>
2022-12-09 00:18:33 +0530

    mycommit

--- src/main/java/com/example/kafkademo/config/Config.java
+++ src/main/java/com/example/kafkademo/config/Config.java
@@ -4,20 +4,20 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@EnableKafka
 @Configuration
 public class Config {
 
     @Bean
     public ProducerFactory<String, String> producerFactoryString() {
         Map<String, Object> configProps = new HashMap<>();
-
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
